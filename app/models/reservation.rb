e3a917b0d1531479374f55978cb5aa204770463f
Juriaan Romijn <jfromijn@gmail.com>
2019-05-20 16:30:51 +0200

    corrected a dual belongs_to entry

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :campsite
-  belongs_to :user
   validates :arrival, presence: true
   validates :departures, presence: true
 end
