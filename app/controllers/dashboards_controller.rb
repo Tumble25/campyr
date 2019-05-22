79279990ff0deff4b6e7e470b374fe2a23994d8b
Juriaan Romijn <jfromijn@gmail.com>
2019-05-22 18:22:32 +0200

    dashboard work

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,5 @@
 class DashboardsController < ApplicationController
-skip_after_action :verify_authorized, only: [:show]
+  skip_after_action :verify_authorized, only: [:show]
   def show
-
   end
 end
--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,7 +1,7 @@
 class Campsite < ApplicationRecord
   belongs_to :user
-  # validates :name, presence: true
-  # validates :address, presence: true
-  # validates :price, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
   mount_uploader :photo, PhotoUploader
 end
